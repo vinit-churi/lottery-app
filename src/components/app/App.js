2cc7ccc4a912223ea6d4b8f86c676654784b0f83
vinit-churi <vinitchuri@hotmail.com>
2020-07-10 17:21:50 +0530

    :smirk: finally done with the issue

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Lottery from '../lottery/Lottery';
 function App() {
   return (
     <div className="App">
-      
+      <Lottery/>
     </div>
   );
 }
